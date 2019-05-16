a096309fc9781cee2a8566cfe098cbc0b2ec7811
cuong.duong <cuong.duong@titancorpvn.com>
2019-05-16 14:35:22 +0700

    add random color for cardview background

--- app/src/main/java/cuongduong/developer/android/keywordstiki/adapter/KeywordAdapter.java
+++ app/src/main/java/cuongduong/developer/android/keywordstiki/adapter/KeywordAdapter.java
@@ -1,7 +1,9 @@
 package cuongduong.developer.android.keywordstiki.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,8 @@ import java.util.List;
 import cuongduong.developer.android.keywordstiki.R;
 
 public class KeywordAdapter extends RecyclerView.Adapter<KeywordAdapter.MyViewHolder> {
+
+    int[] arrayColors = {Color.GREEN, Color.RED, Color.BLUE, Color.DKGRAY, Color.GRAY};
 
     Context context;
     List<String> keywords;
@@ -31,7 +35,9 @@ public class KeywordAdapter extends RecyclerView.Adapter<KeywordAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        int randomColor = (int) Math.round(Math.random() * 4);
         myViewHolder.txtViewKeyword.setText(keywords.get(i));
+        myViewHolder.cardViewKeyword.setBackgroundColor(arrayColors[randomColor]);
     }
 
     @Override
@@ -41,10 +47,12 @@ public class KeywordAdapter extends RecyclerView.Adapter<KeywordAdapter.MyViewHo
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView txtViewKeyword;
+        CardView cardViewKeyword;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             txtViewKeyword = itemView.findViewById(R.id.txt_keyword);
+            cardViewKeyword = itemView.findViewById(R.id.card_view_keyword);
         }
     }
 }
